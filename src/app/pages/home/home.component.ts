d28c6c199b2405eca663ee6d2db63124259b9350
Cartunduaga123 <52115389+Cartunduaga123@users.noreply.github.com>
2024-07-23 18:04:38 -0500

    pluralizar items

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { Task } from './../../models/task.model'
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  tasks = signal<Task[]>([
+    {
+      id: Date.now(),
+      title: 'Crear Proyecto',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Componentes',
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: 'Crear Backend',
+      completed: false
+    }
+    
+  ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.addTask(newTask)
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index));
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, position) => {
+        if(position === index){
+          return {
+            ...task,
+            completed: !task.completed
+          }
+        }
+        return task;
+      })
+    })
+  }
+}
---
+++ src/app/pages/labs/labs.component.ts
@@ -0,0 +1,57 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = 'Hola';
+  tasks = signal([
+    'Instalar el Angular CLI',
+    'Crear Proyecto',
+    'Crear componentes'
+  ]);
+
+  name = signal('camilo');
+  age= '25';
+
+  disabled = true;
+
+  img = 'https://w3schools.com/howto/img_avatar.png';
+  person = signal({
+    name: 'Camilo',
+    age: 5,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  })
+
+  clickHandler() {
+    alert('Hola')
+  }
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value)
+  }
+
+  changeAge(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update(prevState => {
+      return {
+        ...prevState,
+        age: parseInt(newValue, 10)
+      }
+    });
+  }
+  
+}
